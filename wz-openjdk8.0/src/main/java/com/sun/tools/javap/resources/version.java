f3d40374b40471b0ffaaf881951c72e55cc6ac8e
zhouxinjian <zhouxinjian@xiaomi.com>
2020-09-11 12:10:50 +0800

    add jdk7.0&openjdk8.0

---
+++ wz-openjdk8.0/src/main/java/com/sun/tools/javap/resources/version.java
@@ -0,0 +1,11 @@
+package com.sun.tools.javap.resources;
+
+public final class version extends java.util.ListResourceBundle {
+    protected final Object[][] getContents() {
+        return new Object[][] {
+            { "full", "1.8.0_191-1-ojdkbuild-b12" },
+            { "jdk", "1.8.0_191" },
+            { "release", "1.8.0_191-1-ojdkbuild" },
+        };
+    }
+}
